6fc0bd0da7de57a10646dee87a5b54498bf53c72
종철 이 <mylife68@nate.com>
2019-03-09 09:54:13 +0900

    연산자오버로딩

---
+++ OperatorOverloadingExam/OperatorOverloadingExam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace OperatorOverloadingExam{
+    class AddClass { 
+        public static AddClass operator +(AddClass op1, AddClass op2)
+        {
+            AddClass a = new AddClass();
+            a.val = op1.val + op2.val;
+            return a;
+        }
+        public int val { get; set; }  //자동구현속성
+    }
+    class Program    {
+        static void Main(string[] args)     {
+            AddClass a1 = new AddClass(); a1.val = 1;
+            AddClass a2 = new AddClass(); a2.val = 2;
+            AddClass a3 = a1 + a2;
+            Console.WriteLine(a3.val);
+        }
+    }
+}
